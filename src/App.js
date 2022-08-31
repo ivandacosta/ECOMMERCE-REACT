d255fef19948a9296632ba0dbf6c6819a9f45ba6
ivandacosta <ivandacosta11@hotmail.com>
2022-08-31 16:13:39 -0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-
+import React from 'react';
 import './App.css';
 import Header from './components/header/header';
 import ItemCount from './components/ItemCount/itemcount';
 
 function App() {
-
   const onAdd = (counter) => {
-    console.log(`Tienes ${counter} productos`)
-  }
+    console.log(`Tienes ${counter} productos`);
+  };
 
   return (
     <div className="App">
       <Header />
-      <ItemCount initial={1} stock={5} onAdd={onAdd}/>
+      <ItemCount initial={1} stock={5} onAdd={onAdd} />
     </div>
   );
 }
