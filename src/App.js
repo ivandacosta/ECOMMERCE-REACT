e698293f2265eb8782c8048d3f376f12d2459206
ivandacosta <ivandacosta11@hotmail.com>
2022-09-01 00:43:01 -0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ItemCount from './components/ItemCount/itemcount';
 function App() {
   const onAdd = (counter) => {
     console.log(`Tienes ${counter} productos`);
+    alert(`Tienes ${counter} productos en la canasta`);
   };
 
   return (
